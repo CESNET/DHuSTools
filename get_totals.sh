22205215247113dc71910423042bbd85a535552f
Zdenek Sustr <git@sustr.net>
2018-04-06 09:18:52 +0200

    Support dowloading whole lists, not only totals

--- get_totals.sh
+++ get_totals.sh
@@ -3,17 +3,22 @@
 NUM=14
 LAST=1
 ATTRIBUTE="CreationDate"
+LIST=0
+PREFIX=""
 
-while getopts "hn:u:tic" opt; do
+while getopts "hn:u:ticlp:" opt; do
   case $opt in
 	h)
 		printf "Check total files for a given day at a DHuS endpoint \n\nUsage:\n
 \t-h      \tDisplay this help\n \
-\t-n <num>\tNumber of days to count back this help\n \
-\t-t      \tAlso include TODAY
-\t-i      \tSearch by INGESTION Date (default Creation)
-\t-c      \tSearch by CONTENT Date (default Creation)
-\t-u <str>\tuser:password to use accessing the remote site. This is passed directly to curl.\n"
+\t-n <num>\tNumber of days to count back (default 14)\n \
+\t-p <str>\tName prefix (Platform: S1, S2, or S3)\n \
+\t-t      \tAlso include TODAY\n \
+\t-i      \tSearch by INGESTION Date (default Creation)\n \
+\t-c      \tSearch by CONTENT Date (default Creation)\n \
+\t-l      \tSuppress normal output, list ALL products matching criteria\n \
+\t-u <str>\tuser:password to use accessing the remote site.\n \
+\t\t\tThis is passed directly to curl.\n\n"
 		exit 0
 		;;
 	u)
@@ -31,6 +36,12 @@ while getopts "hn:u:tic" opt; do
 	c)
 		ATTRIBUTE="ContentDate"
 		;;
+	l)
+		LIST=1
+		;;
+	p)
+		PREFIX="%20and%20startswith(Name,%27${OPTARG}%27)"
+		;;
   esac
 done
 
@@ -38,11 +49,11 @@ shift $(($OPTIND - 1))
 URL=$1
 
 
-echo "Counting $NUM days back"
-
 NOW=`date -d 'yesterday 00:00:00' "+%s"`
 let START=$NOW-$NUM*86400+$LAST*86400
 
+
+get_totals() {
 
 for i in `seq 1 $NUM`; do
 	SSTRING=`date -d @$START "+%Y-%m-%dT%H:%M:%S.000"`
@@ -52,7 +63,7 @@ for i in `seq 1 $NUM`; do
 	MSTRING=`date -d @$START "+%Y-%m-%d"`
 
 	printf "\n$MSTRING,"
-	curl -u "$UPWD" ${URL}/odata/v1/Products//%24count?%24filter=${ATTRIBUTE}%20gt%20datetime%27${SSTRING}%27%20and%20${ATTRIBUTE}%20lt%20datetime%27${ESTRING}%27
+	curl -u "$UPWD" ${URL}/odata/v1/Products//%24count?%24filter=${ATTRIBUTE}%20gt%20datetime%27${SSTRING}%27%20and%20${ATTRIBUTE}%20lt%20datetime%27${ESTRING}%27${PREFIX}
 	if [ $? -gt 0 ]; then
 		break
 	fi
@@ -60,6 +71,34 @@ for i in `seq 1 $NUM`; do
 done
 
 printf "\n"
+}
+
+get_list() {
+	SSTRING=`date -d @$START "+%Y-%m-%dT%H:%M:%S.000"`
+	PAGESIZE=100
+	SKIP=0
+
+	
+
+	let COUNT=$PAGESIZE+1
+	while [ $COUNT -gt $PAGESIZE ]; do
+		COUNT=0
+		SEG=$(curl -sS -u "$UPWD" ${URL}/odata/v1/Products?%24format=text/csv\&%24select=Name,${ATTRIBUTE}\&%24skip=$SKIP\&%24top=$PAGESIZE\&%24filter=${ATTRIBUTE}%20gt%20datetime%27${SSTRING}%27${PREFIX})
+		while read -r line; do
+			if [ $COUNT -ne 0 ]; then
+				echo $line;
+			fi
+			let COUNT=$COUNT+1
+		done <<< $SEG
+		let SKIP=$SKIP+$PAGESIZE
+	done
+}
+
+if [ $LIST -eq 1 ]; then
+	get_list
+else
+	get_totals
+fi
 
 
 exit 0
