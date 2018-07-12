beeb6bc6a2893b08c5a721eb2357497e9eecd834
Zdenek Sustr <sustr4@cesnet.cz>
2018-07-12 17:54:02 +0200

    Keep track of recently identified files

--- gen_l2_list.sh
+++ gen_l2_list.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 WRKD="/tmp"
+VARTMP="/var/tmp/gen_l2_list"
 NETRCOPT="-n"
+TODAY=`date -d "today" +%Y-%m-%d`
 FROM=`date -d "yesterday-30 days" +%Y-%m-%d`
 VERBOSE=0
+NDAYS=3
 
-while getopts "hvn:f:u:" opt; do
+while getopts "hvn:f:u:p:" opt; do
   case $opt in
         h)
                 printf "Generate a list of L1 products (Sentinel2) that do not have a matching L2A product.\n\nUsage:\n
@@ -13,6 +16,7 @@ while getopts "hvn:f:u:" opt; do
 \t-n <str>\tpath to an altarnative .netrc file (default ~/.netrc)\n \
 \t-u <str>\tusername:password to be used instead of .netrc\n \
 \t-f <Y-M-D>\tStarting date (default ${FROM})\n \
+\t-p <num>\tdisregard files already returned within past 'p' days (default ${NDAYS})\n \
 \t-v      \tVerbose: May add more output, preserves working directory\n \
 \n\n"
                 exit 0
@@ -26,6 +30,9 @@ while getopts "hvn:f:u:" opt; do
         f)
 		FROM="${OPTARG}"
                 ;;
+        p)
+		NDAYS="${OPTARG}"
+                ;;
         v)
 		VERBOSE=1
                 ;;
@@ -34,6 +41,13 @@ done
 
 shift $(($OPTIND - 1))
 URL=$1
+
+debug() {
+	MESSAGE=$@
+	if [ $VERBOSE -gt 0 ]; then
+		>&2 printf "$MESSAGE"
+	fi
+}
 
 get_list() {
         SSTRING="${FROM}T00:00:00.000"
@@ -65,6 +79,7 @@ fi
 PWD=`pwd`
 mkdir -p "${WRKD}/l2comp.$$"
 cd "${WRKD}/l2comp.$$"
+mkdir -p "${VARTMP}"
 
 ##########################################
 # Step 10: List L1C files
@@ -90,20 +105,34 @@ cat l2raw.csv | sed 's/,.*//' | sed 's/_MSIL2A_/_MSIL1C_/' > l2list.csv
 grep -v -f l2list.csv l1list.csv > l1only.csv
 
 ##########################################
-# Step 50: Output results
+# Step 50: Make results
 
-grep -f l1only.csv l1raw.csv | sed 's/.*,\([a-z0-9\-]*\)/\/odata\/v1\/Products\(\%27\1%27\)\/\$value/' > product_list.txt
+grep -f l1only.csv l1raw.csv | sed 's/.*,//' > product_list.txt
+
+if [ $NDAYS -gt 0 ]; then
+	for i in `seq 0 $NDAYS`; do
+		THEDAY=`date -d "${TILL}-${i} days" +%Y-%m-%d`
+		debug "Removing products found on ${THEDAY} (using list in \"${VARTMP}/${THEDAY}\")\n"
+		if [ -f "${VARTMP}/${THEDAY}" ]; then
+			grep -v -f "${VARTMP}/${THEDAY}" product_list.txt > product_list.short.txt
+			mv -f product_list.short.txt product_list.txt
+		fi
+	done
+fi
+
 
 ##########################################
 # Step 60: Output results
 
+cat product_list.txt >> "${VARTMP}/${TODAY}"
 cat product_list.txt
 
+debug "A total of `wc -l product_list.txt | awk '{ print $1 }'` products found for processing.\nFound `wc -l l1raw.csv | awk '{ print $1 }'` L1 and `wc -l l2raw.csv | awk '{ print $1 }'` L2 products for the given period.\n"
 cd "${PWD}"
 
 if [ $VERBOSE -eq 0 ]; then
 	rm -rf "${WRKD}/l2comp.$$"
 else
-	>&2 printf "Working data kept in \"${WRKD}/l2comp.$$\"\nA total of `wc -l product_list.txt | awk '{ print $1 }'` products found for processing.\nFound `wc -l l1raw.csv | awk '{ print $1 }'` L1 and `wc -l l2raw.csv | awk '{ print $1 }'` L2 products for the given period.\n"
+	>&2 printf "Working data kept in \"${WRKD}/l2comp.$$\"\n"
 fi
 
