f269f0ff76b1f367c378c0fd6b9ff73f1dbc510f
Zdenek Sustr <sustr4@cesnet.cz>
2018-07-26 09:18:12 +0200

    Mention gist in README

--- gist/get-daily-volumes.sh
+++ gist/get-daily-volumes.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+# This code snippet cycles throug available DHuS log files and calls
+# the offical statistics script for each one separately.
+# Finally it aggregates the results and cleans them up so that they
+# can be entered "as-is" into any spreadsheet's Pilot Table.
+
 for f in *.log; do
 	NAM=`echo -e $f	| egrep -o '[0-9][0-9\-]*'`
-	printf $NAM
-	if [ -d daily-$NAM ]; then
-		echo already exists
+	printf "$NAM"
+	if [ -d "daily-$NAM" ]; then
+		echo " already exists"
 	else
-		mkdir  daily-$NAM
+		mkdir -p daily-$NAM
 		./DataHubStats_TiB.sh $NAM $NAM daily-$NAM
 	fi
 done
 
 find daily-* -name "retrieved_report*" -exec cat {} > retr-$NAM.csv \;
+
+head -n 1 retr-$NAM.csv > retr-$NAM-pilot.csv
+cat retr-$NAM.csv | egrep -v ";size;|;S[123][ABCD][_.]*;" | sed 's/ to [0-9\-]*//' | sed 's/https:\/\///' | sed 's/\/dhus.*\/v[0-9]//' | sed 's/.copernicus.eu//' >> retr-$NAM-pilot.csv
+
