defd9cc6b1a286a841e7cd95dce8ab6edc020aa1
Zdenek Sustr <sustr4@cesnet.cz>
2018-08-24 09:23:23 +0200

    Get logs Gist

---
+++ gist/get-logs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This gist downloads and combines log files from multiple sources
+
+rsync -v root@be1.dhr.cesnet.cz:/var/dhus/dhus-datahub/logs/dhus-2018-*.log ./
+rsync -v root@be1.dhr.cesnet.cz:/var/dhus2/dhus-datahub/logs/dhus-2018-*.log ./dhus2/
+
+cd dhus2
+for f in *.log; do echo $f; cat $f >> ../$f; sort -o ../$f ../$f; done
+cd ..
+
