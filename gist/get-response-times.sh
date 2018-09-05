db4f684d5ca8a0c02b5478b262d1769ba9d73491
Zdenek Sustr <sustr4@cesnet.cz>
2018-09-05 12:25:02 +0200

    Adding Comments

--- gist/get-response-times.sh
+++ gist/get-response-times.sh
@@ -3,10 +3,16 @@
 # with query response times, suitable for input into spreadsheet
 # pilot tables
 
+# Query records in the log fil do not mention source, giving only
+# a synchronizer number. Therefore we use storage records to find
+# out the source for each synchronizer on a given day.
 grep "successfully synchronized" *.log | sed 's/.*\]\[\([0-9][0-9\-]*\).*Synchronizer\#\([0-9][0-9]*\).*synchronized from.*:\/\/\([^\/]*\).*/s\/\1;\2;\/\1;\3;\//' | sort | uniq > /tmp/syncer.replace.$$
 
+# Print out CSV header
 echo 'Date;Source;Duraion-in-ms'
 
+# Search for query duration records and replace synchronizer numbers
+# with source dnames
 grep -h 'query(Products)' *.log | sed 's/.*\]\[\([0-9][0-9\-]*\).*Synchronizer\#\([0-9][0-9]*\).*done in \([0-9]*\)ms.*/\1;\2;\3/' | sed -f /tmp/syncer.replace.$$
 
 rm -f /tmp/syncer.replace.$$
