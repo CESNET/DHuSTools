93fa6f82fec8ca354bbd95eb80aa75ec6d025eae
Zdenek Sustr <sustr4@cesnet.cz>
2019-01-04 11:15:21 +0100

    Update gist to still work copy+paste
    This is an example, but it may as well be kept useful

--- gist/get-logs.sh
+++ gist/get-logs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This gist downloads and combines log files from multiple sources
 
-rsync -v root@be1.dhr.cesnet.cz:/var/dhus/dhus-datahub/logs/dhus-2018-*.log ./dhus1/
-rsync -v root@be1.dhr.cesnet.cz:/var/dhus2/dhus-datahub/logs/dhus-2018-*.log ./dhus2/
+rsync -v root@be1.dhr.cesnet.cz:/var/dhus/dhus-datahub/logs/dhus-2*.log ./dhus1/
+rsync -v root@be1.dhr.cesnet.cz:/var/dhus2/dhus-datahub/logs/dhus-2*.log ./dhus2/
 
 cd dhus1
 for f in *.log; do echo $f; cat $f > ../$f; done
