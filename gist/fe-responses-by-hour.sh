2fb9ddc77d3f64b87048470edcedfa04d9a3c425
Zdenek Sustr <sustr4@cesnet.cz>
2019-03-12 16:55:25 +0100

    Do not div inline, for now

--- gist/fe-responses-by-hour.sh
+++ gist/fe-responses-by-hour.sh
@@ -2,8 +2,8 @@
 echo Average qeuery response duration by hour of day
 egrep 'Access.*-SUCCESS-' *.log > dwls
 grep 'filter=CreationDate' dwls > queries # Filter out downloads, keep only syncer queries
-cat queries | sed 's/.*\[\(201[89]-[0-9][0-9]-[0-9][0-9]\) \([0-9][0-9]\).*Access[^0-9]*\([.0-9]*\)ms\]\s\(\S\S*\).*/\2 \3 \4/' | sort > queries_hrs.csv
-cat queries_hrs.csv | awk 'START{ last=$1 } { if($1!=lasthr) { print lasthr "," sum "," count "," sum/count; sum=0; count=0 } sum+=$2; count+=1; lasthr=$1; } END { print lasthr "," sum "," count "," sum/count }'
+cat queries | sed 's/.*\[\(201[89]-[0-9][0-9]-[0-9][0-9]\) \([0-9][0-9]\).*Access[^0-9]*\([.0-9]*\)ms\]\s\(\S\S*\).*/\1;\2 \3 \4/' | sort > queries_hrs.csv
+cat queries_hrs.csv | awk 'START{ last=$1 } { if($1!=lasthr) { print lasthr "," sum "," count ; sum=0; count=0 } sum+=$2; count+=1; lasthr=$1; } END { print lasthr "," sum "," count  }'
 
 
 echo Avreage query response duration by originator
