267143d808e7063714e043a53096292f91e40a3c
Zdenek Sustr <git@sustr.net>
2019-03-28 13:17:51 +0100

    Added help and configurability

--- estimate-footprint.sh
+++ estimate-footprint.sh
@@ -21,14 +21,57 @@ fromhuman() {
 	}'
 }
 
-MONTHS=1
-
-FOOTPRINT='footprint:"Intersects(POLYGON((7.125551570329695 56.82293997800775,8.187468460585468 54.83362909344714,10.15067615685664 54.76675883034369,11.614158257713337 54.49298748079582,12.613609448542297 54.57582966128987,12.711769833355858 55.41530314074643,12.756388190089293 55.75824345184793,12.256662594674813 56.656540063131985,10.775333151124743 57.99562854221014,7.125551570329695 56.82293997800775,7.125551570329695 56.82293997800775)))"'
-
+UPWD="-n"
+MONTHS=6
 ENDPOINT='https://scihub.copernicus.eu/dhus'
+OUTPUTPLATFORM=1
+OUTPUTTYPES=1
 
-FIELDS='&select=Id'
+while getopts "hn:e:DTAu:" opt; do
+  case $opt in
+        h)
+                printf "Take DHuS query footprint and get the number and size of files matching that footprint over past months.\n\nUsage:\n \
+\t`basename "$0"` [options] '<footprint>'\n\n \
+\t<footprint>\tDHuS SOLR footprint syntax. This can be copied\n\t\t\tstraight from DHuS' Web interface. It is best\n\t\t\tenclosed in single quotes: ''\n \
+\t-n <int>\tNumber of months to iterate back (Default $MONTHS)\n \
+\t-e <str>\tEndpoint to use for the query\n\t\t\t(Default \"${ENDPOINT}\")\n \
+\t-u <str>\tuser:password to use accessing the remote site.\n \
+\t-D      \tDemo: use demo footprint (Denmark)\n \
+\t-T      \tDo not output per product type, only platform summary\n \
+\t-A      \tDo not output platform summary, only product types\n \
+                \n"
+                exit 0
+                ;;
+        n)
+                MONTHS=$OPTARG
+                ;;
+        e)
+                ENDPOINT=$OPTARG
+                ;;
+        D)
+		FOOTPRINT='footprint:"Intersects(POLYGON((7.125551570329695 56.82293997800775,8.187468460585468 54.83362909344714,10.15067615685664 54.76675883034369,11.614158257713337 54.49298748079582,12.613609448542297 54.57582966128987,12.711769833355858 55.41530314074643,12.756388190089293 55.75824345184793,12.256662594674813 56.656540063131985,10.775333151124743 57.99562854221014,7.125551570329695 56.82293997800775,7.125551570329695 56.82293997800775)))"'
+		>&2 printf "Using demo footprint:\n${FOOTPRINT}\n"
+                ;;
+        T)
+                OUTPUTTYPES=0
+		OUTPUTPLATFORM=1
+                ;;
+        A)
+                OUTPUTTYPES=1
+		OUTPUTPLATFORM=0
+                ;;
+  esac
+done
+if [ "$FOOTPRINT" == "" ]; then
+	shift $(expr $OPTIND - 1 )
+	FOOTPRINT="$@"
+	if [ "$FOOTPRINT" == "" ]; then
+		>&2 echo 'You must specify footprint (or -D for demo)'
+		exit 1
+	fi
+fi
 
+>&2 echo $FOOTPRINT
 
 echo Date Range,Platform,Product Type,Size [B],Size [TiB],Count
 for MONTH in `seq 1 $MONTHS`; do	# Get products per month
@@ -45,7 +88,7 @@ for MONTH in `seq 1 $MONTHS`; do	# Get products per month
 
 		FULLREQUEST=`echo "${ENDPOINT}/search?q=${FOOTPRINT}${POSCONDITION}&start=${START}&rows=100" | sed 's/ /%20/g; s/"/%22/g;'`
 
-		RAWXML=`curl -nsS "$FULLREQUEST"`	# The sed removes all newlines
+		RAWXML=`curl ${UPWD} -sS "$FULLREQUEST"`	# The sed removes all newlines
 		if [ $? -gt 0 ]; then
 			break 2
 		fi
@@ -60,16 +103,21 @@ for MONTH in `seq 1 $MONTHS`; do	# Get products per month
 
 #			echo $PLATFORM,$SIZE
 
-			SIZES["$PLATFORM,all"]=$((${SIZES["$PLATFORM,all"]}+$SIZE))
-			SIZES["$PLATFORM,$PRODUCTTYPE"]=$((${SIZES["$PLATFORM,$PRODUCTTYPE"]}+$SIZE))
-			COUNTS["$PLATFORM,all"]=$((${COUNTS["$PLATFORM,all"]}+1))
-			COUNTS["$PLATFORM,$PRODUCTTYPE"]=$((${COUNTS["$PLATFORM,$PRODUCTTYPE"]}+1))
+			if [ $OUTPUTPLATFORM -gt 0 ]; then
+				SIZES["$PLATFORM,all"]=$((${SIZES["$PLATFORM,all"]}+$SIZE))
+				COUNTS["$PLATFORM,all"]=$((${COUNTS["$PLATFORM,all"]}+1))
+			fi
+
+			if [ $OUTPUTTYPES -gt 0 ]; then
+				SIZES["$PLATFORM,$PRODUCTTYPE"]=$((${SIZES["$PLATFORM,$PRODUCTTYPE"]}+$SIZE))
+				COUNTS["$PLATFORM,$PRODUCTTYPE"]=$((${COUNTS["$PLATFORM,$PRODUCTTYPE"]}+1))
+			fi
 		done < <(echo $RAWXML | sed 's/<\/entry>/<\/entry>\n/g' | grep -v '</feed>')
 
 		START=$(($START+100))
-# XXX		if [ $START -ge $TOTAL ]; then
+		if [ $START -ge $TOTAL ]; then
 			break
-# XXX		fi
+		fi
 	done
 
 	for PLATFORM in "${!SIZES[@]}"; do
