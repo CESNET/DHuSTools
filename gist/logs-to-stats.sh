49a557010845d84c58a41c065c71bf11762abd97
Zdenek Sustr <git@sustr.net>
2019-12-14 18:27:11 +0100

    Skip all XML, KML and GML

--- gist/logs-to-stats.sh
+++ gist/logs-to-stats.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ ! -f "users.pat" ]; then
-	2>&1 printf "This script expects file \"users.pat\" with sed patterns to replace simple username occurrences with csv values. Line by line, for example:\n\ts/^sustr4,/sustr4,Research,Other,Czech Republic,/\nThis is best produced by tweaking the output of:\n\tSELECT login,login,usage,domain,country FROM users;\nIt is possible to start with the \"users.pat\" file in simple tabular TAB-separated format instead. If so this script will attempt to reformat the file on first run.\n"
+	>&2 printf "This script expects file \"users.pat\" with sed patterns to replace simple username occurrences with csv values. Line by line, for example:\n\ts/^sustr4,/sustr4,Research,Other,Czech Republic,/\nThis is best produced by tweaking the output of:\n\tSELECT login,login,usage,domain,country FROM users;\nIt is possible to start with the \"users.pat\" file in simple tabular TAB-separated format instead. If so this script will attempt to reformat the file on first run.\n"
 	exit 1
 fi
 
 egrep '^s/' users.pat >/dev/null
 
 if [ $? -ne 0 ]; then
-	2>&1 printf "Reformatting users.pat\n"
+	>&2 printf "Reformatting users.pat\n"
 	sed -i 's/\t/,\//' users.pat
 	sed -i 's/\t/,/g' users.pat
 	sed -i 's/$/,\//' users.pat
@@ -17,7 +17,7 @@ fi
 
 echo login,usage,domain,country,platform,type,product,size
 
-cat *.log | grep -E '(download.*by.*user.*completed)' | grep -v manifest.safe | grep -v xfdumanifest.xml | awk '{ print $10 " " $6 " " $15 }' | sed "s/['()]//g" | \
+cat *.log | grep -E '(download.*by.*user.*completed)' | grep -v manifest.safe | grep -v xfdumanifest.xml | grep -ivE '.[GXK]ML)' | awk '{ print $10 " " $6 " " $15 }' | sed "s/['()]//g" | \
 awk '{
 	user=$1
 	product=toupper(gensub(/-/,"_","g",$2))
